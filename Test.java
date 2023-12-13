c2b33085b1479a4fcf550c4a721a188e435b4615
Harshalcheke69 <126609210+Harshalcheke69@users.noreply.github.com>
2023-12-13 11:11:41 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,10 +1,8 @@
 public class Test{
 
 public static void main(String args[]){
-   System.out.println("Hello I am Harshal");
-  System.out.println("Hello I am Harshal");
-   System.out.println("Hello I am Harshal");
-    System.out.println("Hello I am Harshal");
+   
+    System.out.println("Hello  World");
 
 }
 }
