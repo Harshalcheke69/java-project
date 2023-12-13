a39f475df2c9d5e7ae39ba585b441a7614670465
Harshalcheke69 <126609210+Harshalcheke69@users.noreply.github.com>
2023-12-13 11:04:14 +0530

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,10 @@
+public class Test{
+
+public static void main(String args[]){
+   System.out.println("Hello I am Harshal");
+  System.out.println("Hello I am Harshal");
+   System.out.println("Hello I am Harshal");
+    System.out.println("Hello I am Harshal");
+
+}
+}
